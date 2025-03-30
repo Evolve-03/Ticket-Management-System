ed5bbfe3e34607ecf576f7760492a56f211b5fea
Sai Nikhil <sainikhil.g123@gmail.com>
2025-03-30 22:16:12 +0530

    changes tin settings.py file

--- bug_tracker/bug_tracker/settings.py
+++ bug_tracker/bug_tracker/settings.py
@@ -117,13 +117,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+ALLOWED_HOSTS = ['Ticket-Management-System.onrender.com', '127.0.0.1']
+
 DJANGO_SETTINGS_MODULE='tracker.settings'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
